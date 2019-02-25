0cecf2c68d7402a8b612314495436bf4091260c5
bmanek <41875460+bmanek@users.noreply.github.com>
2019-02-25 16:03:34 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(tidbit)
+    self.knowledge << tidbit
+  end
+
+  def knowledge
+    @knowledge    
+  end
 
 end
